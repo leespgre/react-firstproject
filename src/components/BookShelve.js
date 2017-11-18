a71b3a3bc50622bc190bba68778bc9c572a627bb
leespgre <leespgre@gmail.com>
2017-11-19 01:30:41 +0900

    modify bookshelf

--- src/components/BookShelve.js
+++ src/components/BookShelve.js
@@ -4,7 +4,7 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import ShelfDetails from './ShelfDetails'
-// import * as BooksAPI from '../BooksAPI'
+
 
 const BookShelve = (props) => {
 
